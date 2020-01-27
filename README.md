91180a0a877956ece63b3b5c2870088d67de6a1d
natak0 <bcnatako@gmail.com>
2020-01-27 14:30:09 +0100

    added pagination and graph

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-A library of all Ghibli movies made from Studio Ghibli API https://ghibliapi.herokuapp.com/ and IMDB API. Written in plain JS and CSS.
-
-https://natak0.github.io/ghibli-movies/
+A list of the Rick and Monty characters from the API: https://rickandmortyapi.com/documentation
+1. a page view list of the Rick and Monty characters
+2. a detailed view on the right of the clicked character’s main characteristics (image, name, species, location, and gender)
+3. a chart graph build on the data from the genders of displayed characters. The library used to build a graph is Chart.js https://www.chartjs.org/
+4. Previous and next page navigation to get the lists of available characters
--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,33 @@
-
-//get data
-
-function getContentx(){
-fetch("https://rickandmortyapi.com/api/character")
-			.then(status)
-            .then(response => console.log(response.json()))
-            //.then(json =>{ })
-            }
-
-function getContent(){
-fetch("https://rickandmortyapi.com/api/character")
+var rickAndMorty = {
+apiURL:"https://rickandmortyapi.com/api/character/?page=1",
+next:"",
+previous:"",
+current:"",
+//regular expression to get the number of the current page from the api address
+regex: /[0-9]+/g,
+//get the api data
+getContent: function(url){
+			fetch(url)
 			.then(status)
             .then(response => response.json())
-            .then(json =>{              
+            .then(json =>{  
+            	//get the links for the next and prev pages
+            	info = json.info;
+            	var hrefPrev = info.prev;
+            	var hrefNext = info.next;
+            	this.next=hrefNext;
+            	this.previous=hrefPrev;
+            	
+            	//the number of the current page
+            	this.current = url.match(this.regex);
+            	document.querySelector('.current-page').innerHTML = this.current;
+            	//get the data for the characters  
                 results=json.results
                 for (var item in results){
                     var character = results[item];
                     //create new div and elements
                     var container = document.createElement("div");
-                    container.className += " character-grid";
-
+                    container.className += "character-grid";
 					//add unique attribute to each div
 					container.setAttribute("data-item",item);
                     document.getElementById('character-container-grid').appendChild(container);
@@ -27,40 +35,74 @@ fetch("https://rickandmortyapi.com/api/character")
 					characterLink.className += "character-link";
                     var characterPoster = document.createElement("img");
                     characterPoster.className += "character-poster";
-                    //add gray pixel preload holder for the poster
-                    characterPoster.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7";
-
-
-                    //add elements to container
+	                //add elements to container
 					container.appendChild(characterLink);
                     characterLink.appendChild(characterPoster);
                     container.setAttribute("data-name",character.name);
                     container.setAttribute("data-species",character.species);
                     container.setAttribute("data-location",character.location.name);
                     container.setAttribute("data-gender",character.gender);
-					
                     //add container to the page
                     document.getElementById('character-container-grid').appendChild(container);
-                    //add api data to elements
-                   	var newName = document.createTextNode(character.name); 
+                    //add api data to the image element
                     characterPoster.src = character.image;
-					characterPoster.setAttribute("alt",newName.nodeValue+" poster");
-					//get a value to add to a new URL for details page
-					hrefStr = newName.nodeValue;
-					//search regEX for spaces or ' special characterin the character name and replace it with "-"
-					hrefStr = hrefStr.replace(/\s+|[`'`]/g, '-');
-					//change the string to lowercase
-					hrefStr = hrefStr.toLowerCase();
-					//create URL for clicked element
-					characterLink.setAttribute("href", "#" + hrefStr);
 					//add click event to the container with a character image
-					container.addEventListener('click', showDetails);
+					container.addEventListener('click', this.getDetails);
                 }          
         })
-}
+        //count the character genders after the json response
+        .then((responseJson) => this.genderCount());
+},
+//count the genders in the list and pass the values to build the chart
+genderCount: function(){
+	//get NodeList
+	var list = document.querySelectorAll('.character-grid');
+	//counters
+	var maleNumber = 0;
+	var femaleNumber = 0;
+	var unknownNumber = 0;
+	for (i=0;i<list.length;i++){
+		var item = list[i].dataset.gender;
+		if (item === "Male"){
+			maleNumber = maleNumber +1;
+		}
+		else if (item === "Female"){
+			femaleNumber = femaleNumber +1;	
+		}
+		else if (item === "unknown"){
+			unknownNumber = unknownNumber +1;	
+		}
+	}
+	this.createGraph(maleNumber, femaleNumber, unknownNumber);
+},
 
+//create the chart from chart.js library
+createGraph:function(male, female, unknown) {
+	var ctx = document.getElementById('graph').getContext('2d');
+	var chart = new Chart(ctx, {
+    type: 'bar',
+    //dataset
+    data: {
+        labels: ['Male', 'Female', 'Unknown'],
+        datasets: [{
+            label: 'Gender Character Count',
+            backgroundColor: '#24292e',
+            data: [ male, female, unknown]
+        }]
+    },})
+},
+getDetails: function(e){
+	//get the attribute information from the clicked div 
+	var image = e.target.src;
+	var name = e.currentTarget.getAttribute('data-name');
+	var species = e.currentTarget.getAttribute('data-species');
+	var location = e.currentTarget.getAttribute('data-location');
+	var gender = e.currentTarget.getAttribute('data-gender');
+	//pass the data info to the createDetails function
+	rickAndMorty.createDetails(image, name, species, location, gender);
+},
 //create the detailed information for the selected character
-function createDetails(image, name, species, location, gender){
+createDetails: function(image, name, species, location, gender){
 	var details = document.querySelector('.details');
 	//clear the content of the details div
 	details.innerHTML = "";
@@ -85,7 +127,7 @@ function createDetails(image, name, species, location, gender){
 	container.appendChild(characterSpecies);
     container.appendChild(characterLocation);
     container.appendChild(characterGender);
-
+    //add the text descriptions
     var newName = document.createTextNode("Name: "+name); 
     characterName.appendChild(newName);
 	var newSpecies = document.createTextNode("Species: "+species);
@@ -94,25 +136,38 @@ function createDetails(image, name, species, location, gender){
     characterLocation.appendChild(newLocation);
     var newGender = document.createTextNode("Gender: "+gender);
     characterGender.appendChild(newGender);
-
+},
+//add click events to prev and next buttons
+addPrevNextPage: function(){
+	const buttonPrev = document.querySelector('.prev-page');
+	const buttonNext = document.querySelector('.next-page');
+	buttonPrev.addEventListener('click', event => {
+		//check if the previous link is not empty
+		if (rickAndMorty.previous){
+			this.clearGrid();
+			this.getContent(rickAndMorty.previous)
+		}
+		else {
+			rickAndMorty.previous = this.apiURL}
+		});
+	buttonNext.addEventListener('click', event => {
+		//check if the next link is not empty
+		if (rickAndMorty.next){
+			this.clearGrid();
+			this.getContent(rickAndMorty.next)
+			}
+		else {
+			rickAndMorty.next = this.apiURL}
+		});	
+},
+//clear the DOM in the character container grid and the clicked character details div
+clearGrid: function(){
+	document.getElementById('character-container-grid').innerHTML = "";
+	document.querySelector('.details').innerHTML = "";
+},
 }
-function showDetails(e){
-	//get attribute information from the clicked div 
-	var image = e.target.src;
-	var name = e.currentTarget.getAttribute('data-name');
-	var species = e.currentTarget.getAttribute('data-species');
-	var location = e.currentTarget.getAttribute('data-location');
-	var gender = e.currentTarget.getAttribute('data-gender');
-	//pass the info to the createDetails function
-	createDetails(image, name, species, location, gender);
-}
-
-
 //add event listeners to the menu after the DOM content is loaded
 document.addEventListener("DOMContentLoaded", function(event) {
-	getContent();
+	rickAndMorty.getContent(rickAndMorty.apiURL);
+	rickAndMorty.addPrevNextPage();
 });
-
-
-
-
